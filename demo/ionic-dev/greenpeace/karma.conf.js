1243444a5b0798daaca8114cda3d9d10876165e3
Julian Jacobs <julian.v.jacobs@gmail.com>
2020-05-04 17:58:02 +0200

    Update karma.conf.js

--- demo/ionic-dev/greenpeace/karma.conf.js
+++ demo/ionic-dev/greenpeace/karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-coverage')
+      require('karma-coverage'),
+      require('karma-coveralls')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
