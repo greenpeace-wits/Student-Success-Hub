c9c01f9fd47796f0ecc0f0fc4ba453bf7175cac6
Julian Jacobs <julian.v.jacobs@gmail.com>
2020-05-04 17:27:20 +0200

    set up for coveralls

--- demo/ionic-dev/greenpeace/karma.conf.js
+++ demo/ionic-dev/greenpeace/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-coverage')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -20,7 +21,11 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    coverageReporter: {
+      type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
+      dir: 'coverage/'
+    },
+    reporters: ['progress', 'kjhtml','coverage','coveralls'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
