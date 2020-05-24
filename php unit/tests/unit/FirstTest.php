96e9d80930e0e0c989dce702dd37d3b95958b4ac
Dudu-N <1388586@students.wits.ac.za>
2020-05-24 14:56:01 +0200

    php unit changes

--- php unit/tests/unit/FirstTest.php
+++ php unit/tests/unit/FirstTest.php
@@ -4,11 +4,28 @@ use PHPUnit\Framework\TestCase;
 
 class FirstTest extends TestCase{
     /** @test */
-    public function MultiplicationOfTwoNumbers(){
-        $a = 5;
-        $b = 4;
-        $c = 5 * 4;
-        
-        $this->assertEquals($c, 20);
+    public function testFetchByStdNum()
+    {
+        $stu = new Student();
+        $stu->set_stu_number(4444);
+        $stu->set_course("COMS2015");
+        $stu->set_test1(80);
+        $stu->set_test2(70);
+        $stu->set_tut_test1(75);
+        $stu->set_tut_test2(90);
+
+        $this -> assertEquals([80,70,75,90], $stu->getMarks(4444));
+    }
+
+    public function testInsertMarks()
+    {
+        $stu = new Student();
+        $stu->set_stu_number(0000);
+        $stu->set_course("COMS2015");
+        $stu->set_test1(100);
+        $stu->set_test2(50);
+        $stu->set_tut_test2(60);
+
+        $this -> assertEquals([100,50,70,60], $stu->enterMarks(0000, "tut_test1", 70));
     }
 }
