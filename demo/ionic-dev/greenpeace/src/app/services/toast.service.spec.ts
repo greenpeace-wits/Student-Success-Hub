1e70ccd2b3c6e70800bdba1afa6f9c8d0828deec
Julian Jacobs <julian.v.jacobs@gmail.com>
2020-09-28 15:16:32 +0200

    Update toast.service.spec.ts

--- demo/ionic-dev/greenpeace/src/app/services/toast.service.spec.ts
+++ demo/ionic-dev/greenpeace/src/app/services/toast.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ToastService } from './toast.service';
 
-fdescribe('ToastService', () => {
+describe('ToastService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
