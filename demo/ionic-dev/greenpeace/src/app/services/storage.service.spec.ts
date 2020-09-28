cc2c81db62afb23e9b620347d922660506757b9d
Julian Jacobs <julian.v.jacobs@gmail.com>
2020-09-28 15:19:54 +0200

    Update storage.service.spec.ts

--- demo/ionic-dev/greenpeace/src/app/services/storage.service.spec.ts
+++ demo/ionic-dev/greenpeace/src/app/services/storage.service.spec.ts
@@ -9,4 +9,28 @@ describe('StorageService', () => {
     const service: StorageService = TestBed.get(StorageService);
     expect(service).toBeTruthy();
   });
+
+  it('should get()', () => {
+    const service: StorageService = TestBed.get(StorageService);
+    service.get('key');
+    expect(true).toBeTruthy();
+  });
+
+  it('should store()', () => {
+    const service: StorageService = TestBed.get(StorageService);
+    service.store('key','');
+    expect(true).toBeTruthy();
+  });
+
+  it('should removeItem()', () => {
+    const service: StorageService = TestBed.get(StorageService);
+    service.removeItem('key');
+    expect(true).toBeTruthy();
+  });
+
+  it('should clear()', () => {
+    const service: StorageService = TestBed.get(StorageService);
+    service.clear();
+    expect(true).toBeTruthy();
+  });
 });
